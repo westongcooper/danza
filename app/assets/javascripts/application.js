f2ad56f4912a26821a76d618a67525905a24fa56
Zlerp <zhpdeals@gmail.com>
2015-09-11 02:19:10 -0400

    Styling for Schedule and Scores

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,11 +15,34 @@
 //= require turbolinks
 //= require_tree .
 
+
 function start_js() {
   console.log('fuck yea');
   $(".button-collapse").sideNav();
   $('.parallax').parallax();
   $('.modal-trigger').leanModal();
+
+  $('.card.small').mouseenter(function() {
+      $(this).addClass("redBorder", 5000);
+  });
+  $('.card.small').mouseleave(function() {
+      $(this).removeClass("redBorder", 5000);
+  });
 }
 
 $(document).on('ready page:load', start_js);
+
+// function sticky_relocate() {
+//     var window_top = $(window).scrollTop();
+//     var div_top = $('#navBarSticky').offset().top;
+//     if (window_top > div_top) {
+//         $('#schedList').addClass('stick');
+//     } else {
+//         $('#schedList').removeClass('stick');
+//     }
+// }
+//
+// $(function () {
+//     $(window).scroll(sticky_relocate);
+//     sticky_relocate();
+// });
