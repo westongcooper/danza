84c770f62e4f7e05b14219b7752d9a500060e859
Weston <weston@westoncooper.com>
2015-09-11 20:24:57 -0400

    mailer

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'djpostal@gmail.com',
+  :user_name => 'djpostal',
   :password => 'password123',
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
