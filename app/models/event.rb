a1cb0969002913371fc1cb0e841c9f77547c3e3b
Weston <weston@westoncooper.com>
2015-09-30 19:49:51 -0400

    uploader

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
   require 'carrierwave/orm/activerecord'
 class Event < ActiveRecord::Base
   has_many :pictures, dependent: :destroy
-  mount_uploader :avatar, AvatarUploader
+  mount_uploader :avatar, EventPicUploader
   mount_uploader :pdf, PdfUploader
 end
